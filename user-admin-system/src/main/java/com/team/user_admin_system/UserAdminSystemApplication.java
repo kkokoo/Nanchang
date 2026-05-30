7e75ccade566ed0d2dbbe50b36791384cab2d386
calm1130 <2647955649@qq.com>
2026-05-30 21:20:57 +0800

    1

--- user-admin-system/src/main/java/com/team/user_admin_system/UserAdminSystemApplication.java
+++ user-admin-system/src/main/java/com/team/user_admin_system/UserAdminSystemApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserAdminSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserAdminSystemApplication.class, args);
 	}
-
 }
